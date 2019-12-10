56744ce74c1a8e7fca43498c842b174bd03a179e
rasmibharti313 <rasmibharti313@gmail.com>
2019-12-10 15:25:24 +0530

    add class

---
+++ Asp.netCoreDemo1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Asp.netCoreDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Asp.netCoreDemo1/Test.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asp.netCoreDemo1
+{
+    class Test
+    {
+    }
+}
